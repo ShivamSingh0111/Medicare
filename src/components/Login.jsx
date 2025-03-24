cc31728f758afdb8be4431b7765550bfff4da89f
ShivamSingh0111 <rajputshivamsinghrkt98@gmail.com>
2025-03-25 01:42:31 +0530

    Doctor-Appoinment Assignment completed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -129,7 +129,7 @@ const Login = ({ onLogin }) => {
         </form>
 
         <div className="signup-link">
-          Don't have an account? <a href="/Signup">Sign up</a>
+          Don't have an account? <button onClick={() => navigate('/Signup')}>Sign up</button>
         </div>
       </div>
     </div>
